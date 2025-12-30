e5522702cb091216913e7fbb2f1c84bd14aafe98
Wathsala Vishwajith <vishwa@Wathsalas-MacBook-Pro.local>
2025-12-30 23:58:23 +0530

    Expermenting with classes

--- factory.py
+++ factory.py
@@ -7,3 +7,24 @@ class Dog:
 
 	def speak(self):
 		return "Woof!"
+
+class Cat:
+
+	"""A simple cat class"""
+
+	def __init__(self, name):
+		self._name = name
+
+	def speak(self):
+		return "Meow!"
+
+def get_pet(pet="cat"):
+	"""The Factory Method"""
+	pets = dict(dog=Dog("Hope"), cat=Cat("Peace"))
+	return pets[pet]
+
+d = get_pet("dog")
+print(d.speak())
+
+c = get_pet("cat")
+print(c.speak())
