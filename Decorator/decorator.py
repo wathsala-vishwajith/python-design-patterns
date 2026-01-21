60a3df4013f22e1b1066c865814d54ca15161284
‘Wathsala <64484342+wathsala-vishwajith@users.noreply.github.com>
2026-01-21 08:46:56 +0530

    2 decortors in 1 function

--- Decorator/decorator.py
+++ Decorator/decorator.py
@@ -16,8 +16,27 @@ def make_blink(function):
 
 	return decorator
 
-#Apply the decorator here!
+#Your decorator code to make the text bold goes here
+def make_bold(function):
+	"""Defines the decorator"""
+
+	#This makes the decorator transparent in terms of its name and docstring
+	@wraps(function)
+
+	#Define the inner function
+	def decorator():
+		#Grab the return value of the function being decorated
+		ret = function() 
+
+		#Add new functionality to the function being decorated
+		return "<b>" + ret + "</b>"
+
+	return decorator
+
+#Your code to apply the decorators goes here!
+
 @make_blink
+@make_bold
 def hello_world():
 	"""Original function! """
 
