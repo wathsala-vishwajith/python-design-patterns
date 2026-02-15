7e41b4820b28c8f685f1530497129df16a5fc9b6
‘Wathsala <64484342+wathsala-vishwajith@users.noreply.github.com>
2026-02-15 23:50:09 +0530

    visitor

--- Visitor/visitor.py
+++ Visitor/visitor.py
@@ -2,12 +2,14 @@ class House(object): #The class being visited
 	def accept(self, visitor):
 		"""Interface to accept a visitor"""
 		#Triggers the visiting operation!
+		visitor.visit(self)
 
 	def work_on_hvac(self, hvac_specialist):
 		print(self, "worked on by", hvac_specialist) #Note that we now have a reference to the HVAC specialist object in the house object!
 
 	def work_on_electricity(self, electrician):
 		#Note that we now have a reference to the electrician object in the house object!
+		print(self, "worked on by", electrician)
 
 	def __str__(self):
 		"""Simply return the class name when the House object is printed"""
@@ -31,19 +33,19 @@ class Electrician(Visitor): #Inherits from the parent class, Visitor
 	"""Concrete visitor: electrician"""
 	def visit(self, house):
 		#Note that the visitor now has a reference to the house object
-
+		house.work_on_electricity(self)
 #Create an HVAC specialist
-
+hvac_specialist = HvacSpecialist()
 #Create an electrician
-
+electrician = Electrician()
 
 #Create a house
-
+house = House()
 
 #Let the house accept the HVAC specialist and work on the house by invoking the visit() method
-
+house.accept(hvac_specialist)
 
 #Let the house accept the electrician and work on the house by invoking the visit() method
-
+house.accept(electrician)
 
 
