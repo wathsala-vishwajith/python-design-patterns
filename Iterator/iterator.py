8fdb258e614baf10efed5212f0aeed6ffe9a61a9
‘Wathsala <64484342+wathsala-vishwajith@users.noreply.github.com>
2026-02-16 00:11:54 +0530

    iterator

--- Iterator/iterator.py
+++ Iterator/iterator.py
@@ -6,15 +6,15 @@ def count_to(count):
 
 	#Our built-in iterator
 	#Creates a tuple such as (1, "eins")
-	
+	iterator = zip(range(count), numbers_in_german)
 	
 	#Iterate through our iterable list
 	#Extract the German numbers
 	#Put them in a generator called number
 	for position, number in iterator:
-		
-		#Returns a 'generator' containing numbers in German
-		
-#Let's test the generator returned by our iterator
+		yield number
 
+#Let's test the generator returned by our iterator
+for number in count_to(4):
+	print(number)
 	
